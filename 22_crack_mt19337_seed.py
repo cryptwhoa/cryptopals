88644d79c750cc7a3ae68308e9c158f9b6a61206
cryptwhoa <jeskelds@cs.uoregon.edu>
2015-07-13 01:16:07 -0400

    22 initial commit

---
+++ 22_crack_mt19337_seed.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+import mt19937
+
+def generate_target()
+        time.wait(random.randint(40, 1000))
+        mt19937.srand(int(time.time()))
+        time.wait(random.randint(40, 1000))
+
+        return mt19937.rand()
+
+def main():
+        target = generate_target()
+        
+
+if __name__ == '__main__':
+        main()
