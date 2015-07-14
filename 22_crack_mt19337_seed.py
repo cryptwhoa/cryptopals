d64ead6daecd1d64ec3b9538616c53037fb242a8
cryptwhoa <jeskelds@cs.uoregon.edu>
2015-07-14 00:24:00 -0400

    add analogous reverse leftshift xor helper function

--- 22_crack_mt19337_seed.py
+++ 22_crack_mt19337_seed.py
@@ -17,6 +17,16 @@ def reverse_xor_leftshift_mask(y, shift, nbits, mask):
                 x such that ((x << c) & mask) ^ x = y
         '''
 
+        x = 0
+        for b in range(nbits):
+                if b < shift or not (mask & _BIT_0RIGHT(b)):
+                        x = x | (y & _BIT_0RIGHT(b))
+                elif mask & _BIT_0RIGHT(b):
+                        x = x | ((y & _BIT_0RIGHT(b)) ^ ((x << shift) & _BIT_0RIGHT(b)))
+
+        return x
+
+
 def reverse_xor_rightshift(y, shift, nbits):
         '''
         solves an equation of the form "y = (x >> c) ^ x"
@@ -33,13 +43,13 @@ def reverse_xor_rightshift(y, shift, nbits):
         x = 0
         for b in range(nbits):
                 if b - shift < 0:
-                        x = x | (y & _BIT(b, nbits))
+                        x = x | (y & _BIT_0LEFT(b, nbits))
                 else:
-                        x = x | ((y & _BIT(b, nbits)) ^ ((x >> shift) & _BIT(b, nbits)))
+                        x = x | ((y & _BIT_0LEFT(b, nbits)) ^ ((x >> shift) & _BIT_0LEFT(b, nbits)))
 
         return x
 
-def _BIT(x, nbits):
+def _BIT_0LEFT(x, nbits):
         '''
         helper function, used for calculating masks
 
@@ -49,6 +59,15 @@ def _BIT(x, nbits):
 
         return 1 << (nbits - x - 1)
 
+def _BIT_0RIGHT(x):
+        '''
+        helper function, used for calculating masks
+        
+        generates a value (b_x)(b_x-1)...b1b0 such that b_i = 0 for all i
+        except i=x
+        '''
+
+        return 1 << x
 
 def generate_target():
         time.sleep(random.randint(40, 1000))
@@ -62,6 +81,10 @@ def main():
         for shift in range(1, 16):
                 x = reverse_xor_rightshift(0xbeef, shift, 16)
                 print hex(((x >> shift) ^ x) & 0xffff)
+
+        for shift in range(1, 16):
+                x = reverse_xor_leftshift_mask(0xdead, shift, 16, 0x1234)
+                print hex((((x << shift) & 0x1234) ^ x) & 0xffff)
         
 
 if __name__ == '__main__':
